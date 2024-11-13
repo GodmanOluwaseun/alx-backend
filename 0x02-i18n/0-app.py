8c3011e8063787e14c902837686fc6efecf60868
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-11-13 12:02:31 +0100

    Single route Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,10 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Default route"""
-    return render_template(
-        '0-index.html', title='Welcome to Holberton',
-        header="Hello world"
-    )
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
