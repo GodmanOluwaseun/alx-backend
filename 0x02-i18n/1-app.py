95dab40a8b1b5d339e44dbb593cf0227823794ec
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-11-13 13:05:24 +0100

    Configure locale and timezone, and instantiate Babel.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -20,11 +20,11 @@ app.url_map.strict_slashes = False
 
 babel = Babel(app)
 
-
 @app.route('/')
 def index():
     """Default route"""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
