85c14220fbfbc6e219bde2ee2df623bf95399873
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-11-13 10:46:16 +0100

    Single route Flask app update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """Return page title and header"""
-    return render_template('index.html', title='Welcome to Holberton', header="Hello world")
+    return render_template(
+        'index.html', title='Welcome to Holberton', 
+        header="Hello world"
+    )
+
 
 if __name__ == '__main__':
     app.run()
